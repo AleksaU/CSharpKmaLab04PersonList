a50b59835b81fbfa3018a9167b36aaad77466147
AleksaU <sangen2901@gmail.com>
2019-03-24 12:47:16 +0200

    added managers

---
+++ Tools/StationManager.cs
@@ -0,0 +1,12 @@
+﻿using CSharpKmaLab04PersonList.Models;
+using System.Collections.ObjectModel;
+
+namespace CSharpKmaLab04PersonList.Tools
+{
+    class StationManager
+    {
+        internal static Person CurrentUser { get; set; }
+        public static ObservableCollection<Person> UsersList { get; }
+    }
+}
+
